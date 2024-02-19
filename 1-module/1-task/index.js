f1aee420442d59bf7903bb8d3ee26933f1fa9a4a
antipers <antipers@gmail.com>
2024-02-19 21:38:15 +0300

    module1- task1 completed

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,9 +1,14 @@
 function factorial(n) {
-  for (let index = 0; index <= n; index++) {
-    n *= n - index;
-    
+  if (n === 0 || n === 1) {
+    return n = 1;
   }
-  return n;
+  else {
+    for (let fact = n; fact >= 2; fact--) {
+       n *= (fact - 1);
+    }
+    return n;
+  }
 }
 
-console.log(factorial(4));
+
+//factorial(5)

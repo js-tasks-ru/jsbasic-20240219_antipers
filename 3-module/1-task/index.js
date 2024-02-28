8a1c2e52900bfcba1b76eca763fd8ae2c84d1db2
antipers <antipers@gmail.com>
2024-02-28 13:52:45 +0300

    module3- task1 completed

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,17 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [vasya, petya, masha];
+
+
 function namify(users) {
-  // ваш код...
+  for (let i = 0; i < users.length; i++) {
+    names[i] = users[i].name;
+  }
+  return names
 }
+
+let names = [];
+names = namify(users);
+

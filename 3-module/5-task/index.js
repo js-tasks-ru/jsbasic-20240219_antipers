6cb3a4077b6e436304ba966a942985f86afd27af
antipers <antipers@gmail.com>
2024-03-02 17:35:47 +0300

    module3- task5 done

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,18 @@
 function getMinMax(str) {
-  // ваш код...
+  splitArr = str.split(' ');
+  numArr = splitArr.filter((userObj) => {
+    if (Number.isFinite(+userObj)) {
+      return userObj
+    }
+  });
+  result.min = Math.min(...numArr)
+  result.max = Math.max(...numArr)  
+console.log(`min: ${result.min}, max: ${result.max}`)  
 }
+
+
+const inputData = '1 и -5.8 или 10 хотя 34 + -5.3 и 73';
+let splitArr = [];
+let numArr = [];
+let result = {};
+getMinMax(inputData);

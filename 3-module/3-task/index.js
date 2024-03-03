a8a0f18a81aab17fb0d338511f33f9e7a42ba446
antipers <antipers@gmail.com>
2024-03-04 01:41:24 +0300

    module3- task3 added v2.0

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,28 +1,25 @@
 function camelize(str) {
+  return str
+    .split('-')
+    .map((userObj, i) => i == 0 ? userObj : userObj.slice(0, 1).toUpperCase() + userObj.slice(1))
+      .join('');
+}
+
+let string = '-webkit-transition';
+console.log(camelize(string));
+
+/*
+вариант с костылями
+function camelize(str) {
   let newArr = str.split('-')
   if (str.length === 0) {
     return str;
   }
-  if (string[0] === '-') {
-    for (let index = 0; index < newArr.length; index++) {
-      let currentWord = newArr[index];
-      if (currentWord === '') {
-        continue
-      }
-      let firstLetter = currentWord.slice(0, 1).toUpperCase();
-      let enchWord = firstLetter + currentWord.slice(1);
-      newArr[index] = enchWord;
-    }
-  } else {
-    for (let index = 1; index < newArr.length; index++) {
+      for (let index = 1; index < newArr.length; index++) {
       let currentWord = newArr[index];
       let firstLetter = currentWord.slice(0, 1).toUpperCase();
       let enchWord = firstLetter + currentWord.slice(1);
       newArr[index] = enchWord;
     }
-  }
-  return newArr.join('');
-}
-
-let string = 'list-style-image';
-camelize(string);
+  
+  return newArr.join('');}*/

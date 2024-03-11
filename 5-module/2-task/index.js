8ce777f7ce2e0f5562f087c27bcf42efb2ad1659
antipers <antipers@gmail.com>
2024-03-12 00:22:17 +0300

    5-module 2-task done

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  let redFlag = true;
+  let btn = document.querySelector('.toggle-text-button')
+  let needHideText = document.querySelector('#text')
+  
+  let btnHandler = function () {
+    if (redFlag) {
+      needHideText.hidden = true;
+      redFlag = false;
+    } else {needHideText.hidden = false; redFlag = true}
+  }
+  btn.addEventListener('click', btnHandler)
 }

03e9307e748cf06b0d93ac0f1e46a22a32f7a225
antipers <antipers@gmail.com>
2024-03-11 21:32:40 +0300

    5-module 1-task done

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,9 @@
 function hideSelf() {
-  // ваш код...
+  let btn = document.querySelector(".hide-self-button");
+  btn.addEventListener('click', (event) => { 
+    event.target.hidden = true; });
 }
+
+
+
+

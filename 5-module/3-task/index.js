38fa8bcdeb710aa48bb668840025bf61c76bfe62
antipers <antipers@gmail.com>
2024-03-13 01:47:33 +0300

    5-module 3-task added variant with switch

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -3,43 +3,46 @@ function initCarousel() {
   let arrowLT = document.querySelector('.carousel__arrow_left');
   let inner = document.querySelector('.carousel__inner');
   let counter = 1;
-  let offsetInner= inner.offsetWidth;
+  let offsetInner = inner.offsetWidth;
 
   arrowLT.style.display = 'none';
   arrowRT.style.display = '';
 
   let arrowRtHandler = function () {
-    if (counter === 1) {
-      inner.style.transform = `translateX(-${offsetInner}px)`;
-      counter += 1;
-      arrowLT.style.display = '';
+    switch (counter) {
+      case 1:
+        inner.style.transform = `translateX(-${offsetInner}px)`;
+        counter++;
+        arrowLT.style.display = '';
+        break;
+      case 2:
+        inner.style.transform = `translateX(-${offsetInner * 2}px)`;
+        counter++;
+        break;
+      case 3:
+        inner.style.transform = `translateX(-${offsetInner * 3}px)`;
+        counter++;
+        arrowRT.style.display = 'none';
+        break;
     }
-    else if (counter === 2) {
-      inner.style.transform = `translateX(-${offsetInner *2}px)`;
-      counter += 1;
-    }
-    else if (counter === 3) {
-      inner.style.transform = `translateX(-${offsetInner *3}px)`;
-      counter += 1;
-      arrowRT.style.display = 'none';
-    }
-
   }
 
   let arrowLtHandler = function () {
-    if (counter === 4) {
-      inner.style.transform = `translateX(-${offsetInner *2}px)`;
-      counter -= 1;
-      arrowRT.style.display = '';
-    }
-    else if (counter === 3) {
-      inner.style.transform = `translateX(-${offsetInner}px)`;
-      counter -= 1;
-    }
-    else if (counter === 2) {
-      inner.style.transform = `translateX(0px)`;
-      counter -= 1;
-      arrowLT.style.display = 'none';
+    switch (counter) {
+      case 4:
+        inner.style.transform = `translateX(-${offsetInner * 2}px)`;
+        counter--;
+        arrowRT.style.display = '';
+        break;
+      case 3:
+        inner.style.transform = `translateX(-${offsetInner}px)`;
+        counter--;
+        break;
+      case 2:
+        inner.style.transform = `translateX(0px)`;
+        counter--;
+        arrowLT.style.display = 'none';
+        break;
     }
   }
   arrowRT.addEventListener('click', arrowRtHandler)
@@ -47,4 +50,38 @@ function initCarousel() {
 }
 
 
-// чем заменить много if
+// вариант с if
+
+//let arrowRtHandler = function () {
+// if (counter === 1) {
+//   inner.style.transform = `translateX(-${offsetInner}px)`;
+//   counter += 1;
+//   arrowLT.style.display = '';
+// }
+// else if (counter === 2) {
+//   inner.style.transform = `translateX(-${offsetInner *2}px)`;
+//   counter += 1;
+// }
+// else if (counter === 3) {
+//   inner.style.transform = `translateX(-${offsetInner *3}px)`;
+//   counter += 1;
+//   arrowRT.style.display = 'none';
+// }
+//}
+
+//let arrowLtHandler = function () {
+// if (counter === 4) {
+//   inner.style.transform = `translateX(-${offsetInner * 2}px)`;
+//   counter -= 1;
+//   arrowRT.style.display = '';
+// }
+// else if (counter === 3) {
+//   inner.style.transform = `translateX(-${offsetInner}px)`;
+//   counter -= 1;
+// }
+// else if (counter === 2) {
+//   inner.style.transform = `translateX(0px)`;
+//   counter -= 1;
+//   arrowLT.style.display = 'none';
+// }
+//}

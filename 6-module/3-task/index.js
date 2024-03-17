6bfbee9d545ec561d6826f2ad7a2e0ad9d684a9d
antipers <antipers@gmail.com>
2024-03-18 01:49:41 +0300

    6-module 3-task completed, probably i'll try to enchant code

--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -21,68 +21,70 @@ export default class Carousel {
   `);
     let inner = slider.querySelector('.carousel__inner');
 
-    inner.insertAdjacentHTML('beforeend', this.slides.map(({ name, price, image, id }) => `
+    inner.insertAdjacentHTML('beforeend', this.slides
+      .map(({ name, price, image, id }) => `
   <div class="carousel__slide" data-id="${id}">
     <img src="/assets/images/carousel/${image}" class="carousel__img" alt="slide">
     <div class="carousel__caption">
-      <span class="carousel__price">${price}</span> 
+      <span class="carousel__price">€${price.toFixed(2)}</span> 
       <div class="carousel__title">${name}</div>
       <button type="button" class="carousel__button">
         <img src="/assets/images/icons/plus-icon.svg" alt="icon">
       </button>
       </div>
-      </div>
-  `).join(''))   // ДОБАВИТЬ НОРМАЛЬНУЮ ЦЕНУ
+      </div>`)
+      .join(''))
     this.elem = slider
   }
 
   addEventListeners() {
-
-
     let arrowRT = this.elem.querySelector('.carousel__arrow_right');
     let arrowLT = this.elem.querySelector('.carousel__arrow_left');
     let inner = this.elem.querySelector('.carousel__inner');
     let counter = 1;
     arrowLT.style.display = 'none';
-    console.log(this.slides.length)
+    let len = this.slides.length;
+
+    function checkCounter() {
+      (counter >= (len)) ? arrowRT.style.display = 'none' : arrowRT.style.display = '';
+      (counter === 1) ? arrowLT.style.display = 'none' : arrowLT.style.display = '';
+    }
 
     this.elem.addEventListener('click', (event) => {
       let leftClc = event.target.closest('.carousel__arrow_left');
       let rightClc = event.target.closest('.carousel__arrow_right');
       let offsetInner = inner.offsetWidth;
       if (rightClc) {
-        if (counter === 1) {
-          inner.style.transform = `translateX(-${offsetInner}px)`;
-          arrowLT.style.display = '';
-          counter++;
-        }
-        else if (counter === 2) {
-          inner.style.transform = `translateX(-${offsetInner * counter}px)`;
-          counter++;
-        }
-        else if (counter === 3) {
-          inner.style.transform = `translateX(-${offsetInner * counter}px)`;
-          arrowRT.style.display = 'none';
-          counter++;
-        }
+        counter++;
+        checkCounter();
+        inner.style.transform = `translateX(-${offsetInner * (counter - 1)}px)`;
       }
       if (leftClc) {
-        if (counter === 4) {
-          counter--;
-          inner.style.transform = `translateX(-${offsetInner * (counter - 1)}px)`;
-          arrowRT.style.display = '';
-        }
-        else if (counter === 3) {
-          counter--;
-          inner.style.transform = `translateX(-${offsetInner * (counter - 1)}px)`;
-        }
-        else if (counter === 2) {
-          counter--;
-          inner.style.transform = `translateX(-${offsetInner * (counter - 1)}px)`;
-          arrowLT.style.display = 'none';
-        }
-
+        counter--;
+        checkCounter();
+        inner.style.transform = `translateX(-${offsetInner * (counter - 1)}px)`;
       }
-    })
+    });
+
+    
+
+this.elem.addEventListener('click', event => {
+  let cartBtn = event.target.closest('.carousel__button');
+  if (cartBtn) {
+    
+    // let getIdElem= this.elem.querySelectorAll('.carousel__slide');
+    // let getIdFromElem= getIdElem.dataset.id;
+    // console.log(getIdFromElem)
+
+    let cartEvent = new CustomEvent("product-add", {
+      detail: this.slides[counter-1].id,
+      bubbles: true,
+    });
+    this.elem.dispatchEvent(cartEvent);
+  }
+});
+this.elem.addEventListener("product-add", () => {
+  console.log('Товар добавлен в корзину:' + event.detail);
+});
   }
 }
